c8c8a2255143afcc835f057b56aedc2bcb1cfc94
Julia Smitjes <julia.s@live.nl>
2026-07-07 15:02:06 +0200

    setup

---
+++ frontend/components/GuideForm.tsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+
+const [discipline, setDiscipline] = useState("");
+const [environment, setEnvironment] = useState("");
+const [task, setTask] = useState("");
+const [language, setLanguage] = useState("English");
