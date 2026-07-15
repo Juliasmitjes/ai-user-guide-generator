8d577a9ea612e31d69a023cf04601fa992c7093e
Julia Smitjes <julia.s@live.nl>
2026-07-15 22:29:01 +0200

    openai

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Form, File, UploadFile
 from typing import List
 from app.models.guide_request import GuideRequest
-from app.services.gemini_service import GeminiService
+from app.services.openai_service import OpenAIService
 
 router = APIRouter()
 
-service = GeminiService()
+service = OpenAIService()
 
 
 @router.post("/generate-guide")
---
+++ backend/app/services/openai_service.py
@@ -0,0 +1,55 @@
+import os
+import base64
+
+from dotenv import load_dotenv
+from openai import OpenAI
+from typing import cast
+from openai.types.responses import ResponseInputParam
+
+from app.prompts.guide_prompt import build_prompt
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+
+class OpenAIService:
+
+    async def generate(self, request, screenshots):
+
+        prompt = build_prompt(request)
+
+        contents = [
+            {
+                "type": "input_text",
+                "text": prompt,
+            }
+        ]
+
+        for screenshot in screenshots:
+            image_bytes = await screenshot.read()
+
+            base64_image = base64.b64encode(image_bytes).decode("utf-8")
+
+            contents.append(
+                {
+                    "type": "input_image",
+                    "image_url": f"data:{screenshot.content_type};base64,{base64_image}",
+                }
+            )
+
+        print(contents)
+        print(f"Aantal onderdelen: {len(contents)}")
+
+        response = client.responses.create(
+            model="gpt-4.1",
+            input=cast(ResponseInputParam, contents),
+        )
+
+        print(response.output_text)
+
+        return {
+            "manual": response.output_text
+        }
