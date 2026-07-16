061b62e3c7831db123b12da35a6bcc5f459ad2fa
Julia Smitjes <julia.s@live.nl>
2026-07-16 22:44:11 +0200

    change fetch

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -105,7 +105,7 @@ export default function Home() {
     });
 
     const response = await fetch(
-      "http://127.0.0.1:8000/generate-guide",
+      `${process.env.NEXT_PUBLIC_API_URL}/generate-guide`,
       {
         method: "POST",
         body: formData,
