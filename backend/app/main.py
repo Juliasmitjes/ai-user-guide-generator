dc2dc6dcc6b850b906f64926dc47573b5f3cdeff
Julia Smitjes <julia.s@live.nl>
2026-07-17 08:39:44 +0200

    cahnge url

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-    "https://ai-user-guide-generator.vercel.app"],
+    "https://work-instruction-generator.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
